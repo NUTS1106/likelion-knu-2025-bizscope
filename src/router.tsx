65564aeb8c0359fa12b9b91f5f1bfde67b161c45
heyrimm <158049950+heyrimm@users.noreply.github.com>
2025-08-25 16:33:10 +0900

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -4,31 +4,36 @@ import MapPage from "./Pages/MapPage";
 import ChartPage from "./Pages/ChartPage";
 import App from "./App";
 import NotFound from "./Pages/NotFound";
+import UserDashboard from "./Pages/UserDashboard"; // 👈 추가
 
-const router=createBrowserRouter([
-    {
-        path:'/',
-        element:<App/>,
-        children:[
-            {
-                path:'',
-                element:<Home/>
-            },
-            {
-                path:'chart',
-                element:<ChartPage/>
-            },
-            {
-                path:'map',
-                element:<MapPage/>
-            },
-            {
-                path:'logout',
-                element:<h1>logout</h1>
-            },
-            { path: "*", element: <NotFound /> },
-        ]
-    },
-])
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "chart",
+        element: <ChartPage />,
+      },
+      {
+        path: "map",
+        element: <MapPage />,
+      },
+      {
+        path: "dashboard",          //UserDashboard 경로 추가
+        element: <UserDashboard />,
+      },
+      {
+        path: "logout",
+        element: <h1>logout</h1>,
+      },
+      { path: "*", element: <NotFound /> },
+    ],
+  },
+]);
 
 export default router;
